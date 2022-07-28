b27400368c969abf24ebc72d2d56cbef04ec5b76
MeirDi <321mid@gmail.com>
2022-07-28 17:05:52 +0300

    fix json bug

--- Runners/TelegramWebRunner/Program.cs
+++ Runners/TelegramWebRunner/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 
 var app = builder.Build();
 
